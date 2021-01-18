99bb5c592a436eb20ae4a7a58d509fd84a7f31a4
Hechanglin <542177932@qq.com>
2021-01-18 15:29:27 +0800

    feat(getSubsets):文件名重命名

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Graph } from './graph';
 export { recursiveFib, dpFib } from './fibonacci';
 export { commonSubStr, LCS } from './common-sub-str';
-export { getSubsets } from './getSubsets';
+export { getSubsets } from './get-subsets';
