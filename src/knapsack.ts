00483a844e21448da4aca425cbfc8bbb24e46f4e
Hechanglin <542177932@qq.com>
2021-01-18 16:17:37 +0800

    feat(knapsack):背包问题 简单算法

--- src/knapsack.ts
+++ src/knapsack.ts
@@ -1,8 +1,33 @@
-// interface item {
-//   value: number;
-//   size: number;
-// }
+import { getSubsets } from './get-subsets';
+import { max } from './max-min';
 
-// export function knapsack(capacity: number, item: item[]): void {
-//   for (let i = 0; i < item.length; i++) {}
-// }
+interface item {
+  value: number;
+  size: number;
+}
+
+/**
+ * 背包问题 简单算法
+ * @param capacity -背包容量
+ * @param {Object[]} items -物品
+ * @param {number} items[].size -物品尺寸
+ * @param {number} items[].value -物品价值
+ * @returns {number}
+ */
+export function knapsackSimple(capacity: number, items: item[]): number {
+  const subsets = getSubsets(items);
+  const res = subsets
+    .map((n) =>
+      n.reduce(
+        (preValue, item) => {
+          return { value: item.value + preValue.value, size: item.size + preValue.size };
+        },
+        { value: 0, size: 0 },
+      ),
+    )
+    .filter((n) => n.size <= capacity);
+
+  const maxValue = max(res, (a, b) => (a.value > b.value ? a : b)).value;
+
+  return maxValue;
+}
---
+++ test/knapsack.test.ts
@@ -0,0 +1,40 @@
+import { knapsackSimple } from '../src/index';
+
+[knapsackSimple].forEach((fn) => {
+  test(fn.name, () => {
+    expect(
+      fn(5, [
+        { value: 3, size: 4 },
+        { value: 7, size: 5 },
+      ]),
+    ).toBe(7);
+
+    expect(
+      fn(6, [
+        { value: 3, size: 1 },
+        { value: 7, size: 5 },
+        { value: 7, size: 8 },
+        { value: 7, size: 9 },
+      ]),
+    ).toBe(10);
+
+    expect(
+      fn(11, [
+        { value: 3, size: 1 },
+        { value: 7, size: 5 },
+        { value: 12, size: 8 },
+        { value: 15, size: 9 },
+      ]),
+    ).toBe(18);
+
+    expect(
+      fn(16, [
+        { value: 4, size: 3 },
+        { value: 5, size: 4 },
+        { value: 10, size: 7 },
+        { value: 11, size: 8 },
+        { value: 13, size: 9 },
+      ]),
+    ).toBe(23);
+  });
+});
