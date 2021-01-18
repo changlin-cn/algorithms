0cc442f64617ca82cb3173e29edae9747f1cc41f
Hechanglin <542177932@qq.com>
2021-01-18 15:21:53 +0800

    refactor(getSubsets):独立抽取到一个文件

---
+++ src/getSubsets.ts
@@ -0,0 +1,39 @@
+/**
+ *获取集合的（某个长度的？）子集
+ * @param {any[]}arr -数组
+ * @param {number}[n] -只获取某个长度的子集 不传此参数则获取所有子集
+ */
+export function getSubsets<T>(arr: T[], n?: number): T[][] {
+  const res: { subset: T[]; lastIndex: number }[] = arr.map((n, i) => ({ subset: [n], lastIndex: i }));
+
+  if (n) {
+    while (res.length) {
+      const current = res[0];
+      if (current.subset.length === n) {
+        break;
+      }
+      res.shift();
+      for (let i = current.lastIndex + 1; i < arr.length; i++) {
+        res.push({
+          subset: current.subset.concat(arr[i]),
+          lastIndex: i,
+        });
+      }
+    }
+  } else {
+    for (let j = 0; j < res.length; j++) {
+      const current = res[j];
+      if (current.subset.length === arr.length) {
+        break;
+      }
+      for (let i = current.lastIndex + 1; i < arr.length; i++) {
+        res.push({
+          subset: current.subset.concat(arr[i]),
+          lastIndex: i,
+        });
+      }
+    }
+  }
+
+  return res.map((n) => n.subset);
+}
