cb67f28ad0aa6411330810eab738bc97d4407624
Hechanglin <542177932@qq.com>
2021-01-19 17:11:15 +0800

    feat(sort):添加排序算法时间比较

--- src/sort/index.ts
+++ src/sort/index.ts
@@ -1,3 +1,6 @@
+/**
+ * 排序算法 堆排序和计数排序（对数据源有要求 ）效率较高
+ */
 export { bubbleSort } from './bubble-sort';
 export { insertionSort } from './insertion-sort';
 export { selectionSort } from './selection-sort';
